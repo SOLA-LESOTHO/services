3b8112b33a3b9a29b85c299b49aee808de60ce61
Alexander Solovov <solovov@gmail.com>
2013-05-07 16:39:59 +0200

    Rights export feature modified as per requirements from financial system.

--- ejbs/search/src/main/java/org/sola/services/ejb/search/repository/entities/RightsExportResult.java
+++ ejbs/search/src/main/java/org/sola/services/ejb/search/repository/entities/RightsExportResult.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.sola.services.ejb.search.repository.entities;
 
 import java.math.BigDecimal;
@@ -15,159 +11,90 @@ public class RightsExportResult extends AbstractReadOnlyEntity {
     public static final String PARAM_DATE_TO = "dateTo";
     public static final String PARAM_RIGHT_TYPE = "rightType";
     public static final String SEARCH_QUERY =
-            "SELECT DISTINCT b.id AS ba_unit_id, b.name_firstpart, b.name_lastpart, "
-            + "(SELECT size FROM administrative.ba_unit_area WHERE ba_unit_id = b.id AND type_code = 'officialArea' LIMIT 1) AS area, "
-            + "r.id AS right_id, r.type_code AS right_type, r.registration_date, r.expiration_date, r.amount, "
-            + "(SELECT string_agg(COALESCE(p.name, '') || ' ' || COALESCE(p.last_name, ''), ',') "
-            + "  FROM administrative.party_for_rrr pr "
-            + "  INNER JOIN party.party p ON pr.party_id = p.id WHERE pr.rrr_id = r.id) AS owners, "
-            + "p.id AS applicant_id, p.name AS applicant_name, p.last_name AS applicant_last_name, "
-            + "ad.description AS applicant_address, p.phone AS applicant_phone, p.mobile AS applicant_mobile, "
-            + "p.email AS applicant_email, p.id_number AS applicant_id_number, p.id_type_code AS applicant_id_type_code "
-            + "FROM administrative.ba_unit b INNER JOIN "
+            "SELECT DISTINCT b.id AS ba_unit_id, r.status_code AS right_status, r.status_change_date AS right_status_date, "
+            + "r.id AS right_id, r.type_code AS right_type, r.registration_date, r.expiration_date, r.amount, r.registration_number, "
+            + "r.nr AS right_tracking_number, (SELECT string_agg(COALESCE(p.name, '') || ' ' || COALESCE(p.last_name, ''), ',') "
+            + "FROM administrative.party_for_rrr pr INNER JOIN party.party p ON pr.party_id = p.id WHERE pr.rrr_id = r.id) AS owners, "
+            + "payee.payee_id, payee.payee_name, payee.payee_last_name, payee.payee_address, payee.payee_phone, payee.payee_mobile, "
+            + "payee.payee_email, payee.payee_id_number, payee.payee_id_type_code, payee.payee_birth_date, payee.payee_gender, "
+            + "(SELECT name_firstpart || '-' || name_lastpart FROM cadastre.cadastre_object WHERE id = baco.spatial_unit_id LIMIT 1) AS parcel_number, "
+            + "(SELECT size FROM cadastre.spatial_value_area WHERE spatial_unit_id = baco.spatial_unit_id AND type_code='officialArea' LIMIT 1) AS area "
+            + "FROM (administrative.ba_unit b INNER JOIN "
             + "(administrative.rrr r LEFT JOIN "
-            + "  (transaction.transaction t INNER JOIN "
-            + "    (application.service s INNER JOIN "
-            + "      (application.application a INNER JOIN "
-            + "        (party.party p LEFT JOIN address.address ad ON p.address_id = ad.id) "
-            + "        ON a.contact_person_id = p.id) "
-            + "      ON s.application_id = a.id) "
-            + "    ON t.from_service_id = s.id) "
-            + "  ON r.transaction_id = t.id) "
-            + "ON b.id = r.ba_unit_id "
-            + "WHERE (r.type_code = #{" + PARAM_RIGHT_TYPE + "} OR #{" + PARAM_RIGHT_TYPE + "} = '') "
-            + "AND b.status_code = 'current' AND r.status_code = 'current' "
-            + "AND r.registration_date BETWEEN #{" + PARAM_DATE_FROM + "} AND #{" + PARAM_DATE_TO + "}";
+            + "  (administrative.party_for_rrr prrr LEFT JOIN "
+            + "       ("
+            + "    SELECT p.id AS payee_id, p.name AS payee_name, p.last_name AS payee_last_name, p.birth_date as payee_birth_date, "
+            + "    ad.description AS payee_address, p.phone AS payee_phone, p.mobile AS payee_mobile, p.gender_code AS payee_gender, "
+            + "    p.email AS payee_email, p.id_number AS payee_id_number, p.id_type_code AS payee_id_type_code "
+            + "    FROM (party.party p INNER JOIN party.party_role pr ON p.id=pr.party_id) "
+            + "    LEFT JOIN address.address ad ON p.address_id = ad.id "
+            + "	    WHERE pr.type_code = 'accountHolder' LIMIT 1 "
+            + "	    ) AS payee ON payee.payee_id = prrr.party_id) "
+            + "     ON r.id = prrr.rrr_id) "
+            + "   ON b.id = r.ba_unit_id) LEFT JOIN administrative.ba_unit_contains_spatial_unit baco ON b.id = baco.ba_unit_id "
+            + "   WHERE (r.type_code = #{" + PARAM_RIGHT_TYPE + "} OR #{" + PARAM_RIGHT_TYPE + "} = '') "
+            + "   AND b.status_code != 'pending' AND (r.status_code = 'current' OR r.status_code = 'historic') "
+            + "   AND r.status_change_date BETWEEN #{" + PARAM_DATE_FROM + "} AND #{" + PARAM_DATE_TO + "}";
     
     @Column(name = "ba_unit_id")
     private String baUnitId;
-    @Column(name = "name_firstpart")
-    private String nameFirstPart;
-    @Column(name = "name_lastpart")
-    private String nameLastPart;
+    @Column(name = "right_status")
+    private String rightStatus;
+    @Column(name = "right_status_date")
+    private Date rightStatusDate;
     @Column(name = "area")
-    private BigDecimal area;
+    private BigDecimal parcelArea;
     @Column(name = "right_id")
     private String rightId;
     @Column(name = "right_type")
     private String rightType;
     @Column(name = "registration_date")
-    private Date registrationDate;
+    private Date rightRegistrationDate;
     @Column(name = "expiration_date")
-    private Date expirationDate;
+    private Date rightExpirationDate;
     @Column(name = "amount")
-    private BigDecimal amount;
+    private BigDecimal paymentAmount;
+    @Column(name="registration_number")
+    private String rightRegistrationNumber;
+    @Column(name="right_tracking_number")
+    private String rightTrackingNumber;
     @Column(name = "owners")
-    private String owners;
-    @Column(name = "applicant_id")
-    private String applicantId;
-    @Column(name = "applicant_name")
-    private String applicantName;
-    @Column(name = "applicant_last_name")
-    private String applicantLastName;
-    @Column(name = "applicant_address")
-    private String applicantAddress;
-    @Column(name = "applicant_phone")
-    private String applicantPhone;
-    @Column(name = "applicant_mobile")
-    private String applicantMobile;
-    @Column(name = "applicant_email")
-    private String applicantEmail;
-    @Column(name = "applicantIdNumber")
-    private String applicantIdNumber;
-    @Column(name = "applicant_id_type_code")
-    private String applicantIdTypeCode;
-
+    private String rightHolders;
+    @Column(name = "payee_id")
+    private String payeeId;
+    @Column(name = "payee_name")
+    private String payeeName;
+    @Column(name = "payee_last_name")
+    private String payeeLastName;
+    @Column(name = "payee_gender")
+    private String payeeGender;
+    @Column(name = "payee_address")
+    private String payeeAddress;
+    @Column(name = "payee_phone")
+    private String payeePhone;
+    @Column(name = "payee_mobile")
+    private String payeeMobile;
+    @Column(name = "payee_email")
+    private String payeeEmail;
+    @Column(name = "payee_id_number")
+    private String payeeIdNumber;
+    @Column(name = "payee_id_type_code")
+    private String payeeIdTypeCode;
+    @Column(name="payee_birth_date")
+    private Date payeeBirthDate;
+    @Column(name="parcel_number")
+    private String parcelNumber;
+    
     public RightsExportResult() {
         super();
     }
 
-    public BigDecimal getAmount() {
-        return amount;
+    public BigDecimal getPaymentAmount() {
+        return paymentAmount;
     }
 
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public String getApplicantAddress() {
-        return applicantAddress;
-    }
-
-    public void setApplicantAddress(String applicantAddress) {
-        this.applicantAddress = applicantAddress;
-    }
-
-    public String getApplicantEmail() {
-        return applicantEmail;
-    }
-
-    public void setApplicantEmail(String applicantEmail) {
-        this.applicantEmail = applicantEmail;
-    }
-
-    public String getApplicantId() {
-        return applicantId;
-    }
-
-    public void setApplicantId(String applicantId) {
-        this.applicantId = applicantId;
-    }
-
-    public String getApplicantIdNumber() {
-        return applicantIdNumber;
-    }
-
-    public void setApplicantIdNumber(String applicantIdNumber) {
-        this.applicantIdNumber = applicantIdNumber;
-    }
-
-    public String getApplicantIdTypeCode() {
-        return applicantIdTypeCode;
-    }
-
-    public void setApplicantIdTypeCode(String applicantIdTypeCode) {
-        this.applicantIdTypeCode = applicantIdTypeCode;
-    }
-
-    public String getApplicantLastName() {
-        return applicantLastName;
-    }
-
-    public void setApplicantLastName(String applicantLastName) {
-        this.applicantLastName = applicantLastName;
-    }
-
-    public String getApplicantMobile() {
-        return applicantMobile;
-    }
-
-    public void setApplicantMobile(String applicantMobile) {
-        this.applicantMobile = applicantMobile;
-    }
-
-    public String getApplicantName() {
-        return applicantName;
-    }
-
-    public void setApplicantName(String applicantName) {
-        this.applicantName = applicantName;
-    }
-
-    public String getApplicantPhone() {
-        return applicantPhone;
-    }
-
-    public void setApplicantPhone(String applicantPhone) {
-        this.applicantPhone = applicantPhone;
-    }
-
-    public BigDecimal getArea() {
-        return area;
-    }
-
-    public void setArea(BigDecimal area) {
-        this.area = area;
+    public void setPaymentAmount(BigDecimal paymentAmount) {
+        this.paymentAmount = paymentAmount;
     }
 
     public String getBaUnitId() {
@@ -178,52 +105,124 @@ public class RightsExportResult extends AbstractReadOnlyEntity {
         this.baUnitId = baUnitId;
     }
 
-    public Date getExpirationDate() {
-        return expirationDate;
+    public BigDecimal getParcelArea() {
+        return parcelArea;
     }
 
-    public void setExpirationDate(Date expirationDate) {
-        this.expirationDate = expirationDate;
+    public void setParcelArea(BigDecimal parcelArea) {
+        this.parcelArea = parcelArea;
     }
 
-    public String getNameFirstPart() {
-        return nameFirstPart;
+    public String getParcelNumber() {
+        return parcelNumber;
     }
 
-    public void setNameFirstPart(String nameFirstPart) {
-        this.nameFirstPart = nameFirstPart;
+    public void setParcelNumber(String parcelNumber) {
+        this.parcelNumber = parcelNumber;
     }
 
-    public String getNameLastPart() {
-        return nameLastPart;
+    public String getPayeeAddress() {
+        return payeeAddress;
     }
 
-    public void setNameLastPart(String nameLastPart) {
-        this.nameLastPart = nameLastPart;
+    public void setPayeeAddress(String payeeAddress) {
+        this.payeeAddress = payeeAddress;
     }
 
-    public String getOwners() {
-        return owners;
+    public Date getPayeeBirthDate() {
+        return payeeBirthDate;
     }
 
-    public void setOwners(String owners) {
-        this.owners = owners;
+    public void setPayeeBirthDate(Date payeeBirthDate) {
+        this.payeeBirthDate = payeeBirthDate;
     }
 
-    public Date getRegistrationDate() {
-        return registrationDate;
+    public String getPayeeEmail() {
+        return payeeEmail;
     }
 
-    public void setRegistrationDate(Date registrationDate) {
-        this.registrationDate = registrationDate;
+    public void setPayeeEmail(String payeeEmail) {
+        this.payeeEmail = payeeEmail;
     }
 
-    public String getRightType() {
-        return rightType;
+    public String getPayeeGender() {
+        return payeeGender;
     }
 
-    public void setRightType(String rightType) {
-        this.rightType = rightType;
+    public void setPayeeGender(String payeeGender) {
+        this.payeeGender = payeeGender;
+    }
+
+    public String getPayeeId() {
+        return payeeId;
+    }
+
+    public void setPayeeId(String payeeId) {
+        this.payeeId = payeeId;
+    }
+
+    public String getPayeeIdNumber() {
+        return payeeIdNumber;
+    }
+
+    public void setPayeeIdNumber(String payeeIdNumber) {
+        this.payeeIdNumber = payeeIdNumber;
+    }
+
+    public String getPayeeIdTypeCode() {
+        return payeeIdTypeCode;
+    }
+
+    public void setPayeeIdTypeCode(String payeeIdTypeCode) {
+        this.payeeIdTypeCode = payeeIdTypeCode;
+    }
+
+    public String getPayeeLastName() {
+        return payeeLastName;
+    }
+
+    public void setPayeeLastName(String payeeLastName) {
+        this.payeeLastName = payeeLastName;
+    }
+
+    public String getPayeeMobile() {
+        return payeeMobile;
+    }
+
+    public void setPayeeMobile(String payeeMobile) {
+        this.payeeMobile = payeeMobile;
+    }
+
+    public String getPayeeName() {
+        return payeeName;
+    }
+
+    public void setPayeeName(String payeeName) {
+        this.payeeName = payeeName;
+    }
+
+    public String getPayeePhone() {
+        return payeePhone;
+    }
+
+    public void setPayeePhone(String payeePhone) {
+        this.payeePhone = payeePhone;
+    }
+
+    public Date getRightExpirationDate() {
+        return rightExpirationDate;
+    }
+
+    public void setRightExpirationDate(Date rightExpirationDate) {
+        this.rightExpirationDate = rightExpirationDate;
+    }
+
+    public String getRightHolders() {
+        return rightHolders;
+    }
+
+    public void setRightHolders(String rightHolders) {
+        this.rightHolders = rightHolders;
     }
 
     public String getRightId() {
@@ -232,5 +231,53 @@ public class RightsExportResult extends AbstractReadOnlyEntity {
 
     public void setRightId(String rightId) {
         this.rightId = rightId;
+    }
+
+    public Date getRightRegistrationDate() {
+        return rightRegistrationDate;
+    }
+
+    public void setRightRegistrationDate(Date rightRegistrationDate) {
+        this.rightRegistrationDate = rightRegistrationDate;
+    }
+
+    public String getRightRegistrationNumber() {
+        return rightRegistrationNumber;
+    }
+
+    public void setRightRegistrationNumber(String rightRegistrationNumber) {
+        this.rightRegistrationNumber = rightRegistrationNumber;
+    }
+
+    public String getRightStatus() {
+        return rightStatus;
+    }
+
+    public void setRightStatus(String rightStatus) {
+        this.rightStatus = rightStatus;
+    }
+
+    public Date getRightStatusDate() {
+        return rightStatusDate;
+    }
+
+    public void setRightStatusDate(Date rightStatusDate) {
+        this.rightStatusDate = rightStatusDate;
+    }
+
+    public String getRightTrackingNumber() {
+        return rightTrackingNumber;
+    }
+
+    public void setRightTrackingNumber(String rightTrackingNumber) {
+        this.rightTrackingNumber = rightTrackingNumber;
+    }
+
+    public String getRightType() {
+        return rightType;
+    }
+
+    public void setRightType(String rightType) {
+        this.rightType = rightType;
     }
 }
