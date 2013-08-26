73fff098a6b4c224f11552075fa03f230b917946
Ntsane Mafereka <maferekan@gmail.com>
2013-08-26 10:51:38 +0200

    removed dead code of fee calculation

--- ejbs/administrative/src/main/java/org/sola/services/ejb/administrative/businesslogic/LeaseFeeUtility.java
+++ ejbs/administrative/src/main/java/org/sola/services/ejb/administrative/businesslogic/LeaseFeeUtility.java
@@ -1,6 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * ******************************************************************************************
+ * Copyright (c) 2013 Food and Agriculture Organization of the United Nations (FAO)
+ * and the Lesotho Land Administration Authority (LAA). All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice,this list
+ *       of conditions and the following disclaimer.
+ *    2. Redistributions in binary form must reproduce the above copyright notice,this list
+ *       of conditions and the following disclaimer in the documentation and/or other
+ *       materials provided with the distribution.
+ *    3. Neither the names of FAO, the LAA nor the names of its contributors may be used to
+ *       endorse or promote products derived from this software without specific prior
+ * 	  written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,STRICT LIABILITY,OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * *********************************************************************************************
  */
 package org.sola.services.ejb.administrative.businesslogic;
 
@@ -211,29 +235,31 @@ public class LeaseFeeUtility extends AbstractEJB{
         
     }
     
-    private BigInteger getGroundRentModulo(BigDecimal groundRent){
+    private BigInteger getStampDutyFactor(BigDecimal groundRent){
         
        
         BigInteger groundRentDivisor = BigInteger.valueOf(100);
         BigInteger groundRentDivident;
-        BigInteger groundRentModulo;
+        BigInteger stampDutyFactor;
         
         groundRentDivident = groundRent.toBigInteger();
         
         //check if ground rent is greater than 100
         if (groundRentDivident.compareTo(groundRentDivisor) == 1) {
-           groundRentModulo = groundRentDivident.mod(groundRentDivisor);
+           stampDutyFactor = groundRentDivident.mod(groundRentDivisor);
            //if value is greater than zero round to next (not nearest) hundred
-           if (groundRentModulo.compareTo(BigInteger.ZERO) == 1){
-               groundRentDivident = groundRentDivident.add(groundRentDivisor).subtract(groundRentModulo);
-               groundRentModulo = groundRentDivident.divide(groundRentDivisor);
+           if (stampDutyFactor.compareTo(BigInteger.ZERO) == 1){
+               groundRentDivident = groundRentDivident.add(groundRentDivisor).subtract(stampDutyFactor);
+               stampDutyFactor = groundRentDivident.divide(groundRentDivisor);
            }
         }
-        else {
-            groundRentModulo = BigInteger.ONE;
+        else if (groundRentDivident.compareTo(BigInteger.ZERO) == 0){
+            stampDutyFactor = BigInteger.ZERO;
+        }else{
+             stampDutyFactor = BigInteger.ONE;
         }
         
-        return groundRentModulo;
+        return stampDutyFactor;
         
     }
     
@@ -304,12 +330,12 @@ public class LeaseFeeUtility extends AbstractEJB{
     public Money calculateDutyOnGroundRent(CadastreObject cadastreObject, Rrr leaseRight){
         
         String landUse = "";
-        String landGrade = "";
+        String landGrade;
         
         BigDecimal groundRent = BigDecimal.ZERO;
-        BigDecimal dutyOnGroundRentValue = BigDecimal.ZERO;
+        BigDecimal dutyOnGroundRentValue;
         
-        BigInteger dutyOnGroundRent = BigInteger.ZERO;
+        BigInteger dutyOnGroundRent;
         BigInteger dutyOnGroundRentFactor = BigInteger.ZERO;
         BigInteger groundRentModulo;
         
@@ -329,7 +355,7 @@ public class LeaseFeeUtility extends AbstractEJB{
             return new Money(BigDecimal.ZERO);
         }
         
-        groundRentModulo = getGroundRentModulo(groundRent);
+        groundRentModulo = getStampDutyFactor(groundRent);
         
         landUseGrade = cadastreEJB.getLandUseGrade(landUse, landGrade);
         
